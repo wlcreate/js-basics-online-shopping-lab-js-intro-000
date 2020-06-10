eea9935dc22e577f069116eace1cc426a387664a
Waverley Leung <waverley.nyc@gmail.com>
2020-06-10 20:43:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,9 +59,17 @@ function removeFromCart(item) {
   }
   if (item != cart[item]) {
     return "That item is not in your cart."
-}
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === 0) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var totalCost = total()
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+    var emptyCart = cart.pop
+    return emptyCart
+  }
 }
