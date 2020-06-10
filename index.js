4332d0503856ccd053e0615a79a114f776e24c15
Waverley Leung <waverley.nyc@gmail.com>
2020-06-10 20:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ function viewCart() {
 
 function total() {
   // write your code here
+  total = 0
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
