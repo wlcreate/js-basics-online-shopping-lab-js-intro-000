935d97bc70a0e03af60872d4ed5ee713c9914d6a
Waverley Leung <waverley.nyc@gmail.com>
2020-06-10 20:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,15 +51,14 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if (item != cart[item]) {
-    return "That item is not in your cart."
-  } else {
     for (let i = 0; i < cart.length; i++) {
       if (item === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
     }
   }
+  if (item != cart[item]) {
+    return "That item is not in your cart."
 }
 }
 
