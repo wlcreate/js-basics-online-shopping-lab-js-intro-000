aa0998ca1cb5b28787b84653b4b2b91c2c8d998b
Waverley Leung <waverley.nyc@gmail.com>
2020-06-10 20:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function placeOrder(cardNumber) {
   if (typeof(cardNumber) === "undefined") {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
     cart = []
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
     return cart
   }
 }
